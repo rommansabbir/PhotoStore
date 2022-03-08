77e969f94805415eb48342d816f765a8c86d4d67
Romman Sabbir <rommansabbir@gmail.com>
2022-03-08 20:58:07 +0600

    Adapter: Use DiffUtil to submit new list backed by Coroutine

--- app/src/main/java/com/rommansabbir/photostore/Adapter.kt
+++ app/src/main/java/com/rommansabbir/photostore/Adapter.kt
@@ -4,20 +4,36 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.rommansabbir.photostore.base.data.PhotoModel
 import com.rommansabbir.photostore.base.executeBodyOrReturnNull
 import com.rommansabbir.photostore.base.loadWithGlide
 import com.rommansabbir.photostore.base.setVisibility
-import com.rommansabbir.photostore.base.showToast
 import com.rommansabbir.photostore.databinding.ContentItemPhotoBinding
 import dagger.hilt.android.qualifiers.ActivityContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @SuppressLint("NotifyDataSetChanged")
 class Adapter @Inject constructor(@ActivityContext val context: Context) :
     RecyclerView.Adapter<PhotoViewHolder>() {
-    val list: MutableList<PhotoModel> = mutableListOf()
+    private val diffUtilsCallBack = object : DiffUtil.ItemCallback<PhotoModel>() {
+        override fun areItemsTheSame(oldItem: PhotoModel, newItem: PhotoModel): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: PhotoModel, newItem: PhotoModel): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+    private var differ: AsyncListDiffer<PhotoModel> = AsyncListDiffer(this, diffUtilsCallBack)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder =
         PhotoViewHolder(
             ContentItemPhotoBinding.inflate(
@@ -32,30 +48,43 @@ class Adapter @Inject constructor(@ActivityContext val context: Context) :
         holder.bindView(position)
     }
 
-
-    fun submitList(list: MutableList<PhotoModel>) {
-        this.list.clear()
-        this.list.addAll(list)
-        this.notifyDataSetChanged()
+    suspend fun addDataSet(dataList: MutableList<PhotoModel>, onSuccess: () -> Unit = {}) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val list: MutableList<PhotoModel> = executeBodyOrReturnNull<MutableList<PhotoModel>> {
+                val previousList =
+                    ArrayList<PhotoModel>(
+                        differ.currentList.toMutableList().toMutableSet().toMutableList()
+                    )
+                previousList.addAll(dataList)
+                return@executeBodyOrReturnNull previousList
+            } ?: mutableListOf()
+            withContext(Dispatchers.Main) {
+                differ.submitList(list)
+                onSuccess.invoke()
+            }
+        }
     }
 
     internal var itemCallback: (model: PhotoModel) -> Unit = {}
 
-    override fun getItemCount(): Int = list.size
+    override fun getItemCount(): Int = differ.currentList.size
 
+    internal fun collectDataSet(): MutableList<PhotoModel> = differ.currentList.toMutableList()
+
+    fun clearDataSet() {
+        this.differ.submitList(null)
+        this.notifyDataSetChanged()
+    }
 }
 
 class PhotoViewHolder(private val binding: ContentItemPhotoBinding, private val adapter: Adapter) :
     RecyclerView.ViewHolder(binding.root) {
     fun bindView(position: Int) {
         executeBodyOrReturnNull {
-            val model = adapter.list[position]
+            val model = adapter.collectDataSet()[position]
             binding.progressBar2.setVisibility(true)
-            binding.imageView.loadWithGlide(model.src?.original ?: "") {
+            binding.imageView.loadWithGlide(model.src?.medium ?: "") {
                 executeBodyOrReturnNull {
-                    it?.let {
-                        showToast(adapter.context, it.message.toString())
-                    }
                     binding.progressBar2.setVisibility(false)
                 }
             }
