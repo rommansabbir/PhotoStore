f2e027d5b8ed8c555594596c8bf386c25f289fac
Romman Sabbir <rommansabbir@gmail.com>
2022-03-08 20:10:17 +0600

    MainActivity -> - Added basic search functionality

--- app/src/main/java/com/rommansabbir/photostore/features/MainActivity.kt
+++ app/src/main/java/com/rommansabbir/photostore/features/MainActivity.kt
@@ -6,9 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.rommansabbir.photostore.Adapter
 import com.rommansabbir.photostore.R
+import com.rommansabbir.photostore.base.customize
 import com.rommansabbir.photostore.base.data.PhotoSearchRequestModel
+import com.rommansabbir.photostore.base.doAfterTextChanged
 import com.rommansabbir.photostore.base.handleFailure
-import com.rommansabbir.photostore.base.showToast
 import com.rommansabbir.photostore.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -16,29 +17,42 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val vm: MainViewModel by viewModels()
+
     private lateinit var binding: ActivityMainBinding
 
     @Inject
     lateinit var adapter: Adapter
 
+    private var currentPage: Int = 1
+    private val perPage: Int = 100
+    private var searchKeyword: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupBinding()
+        setupSearchView()
+
+        /*Default search*/
+        loadImages()
+    }
+
+    private fun setupBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.vm = vm
         binding.lifecycleOwner = this
         binding.recyclerView.adapter = adapter
+    }
 
-        //
+    private fun setupSearchView() {
+        binding.searchView.customize()
+        binding.searchView.doAfterTextChanged { searchKeyword = it;loadImages() }
+    }
+
+    private fun loadImages() {
         vm.setLoading(true)
-        vm.searchPhotos(
-            PhotoSearchRequestModel(
-                1,
-                100,
-                "Nature"
-            ),
+        vm.searchPhotos(getRequestModel(),
             {
                 vm.setLoading(false)
-                showToast(this, it.toString())
                 adapter.submitList(it.photos)
             },
             {
@@ -47,4 +61,11 @@ class MainActivity : AppCompatActivity() {
             }
         )
     }
+
+    private fun getRequestModel(): PhotoSearchRequestModel = PhotoSearchRequestModel(
+        currentPage,
+        perPage,
+        if (searchKeyword.isEmpty()) "Nature" else searchKeyword
+    )
+
 }
