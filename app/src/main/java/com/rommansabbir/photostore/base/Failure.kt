10eb6b861694549e5a5b124be80d53fddb7fa963
Romman Sabbir <rommansabbir@gmail.com>
2022-03-08 18:19:14 +0600

    Base Classes Added:: - Either class to return Failure or <T> object - Failure class to Define and Handle app errors

---
+++ app/src/main/java/com/rommansabbir/photostore/base/Failure.kt
@@ -0,0 +1,25 @@
+package com.rommansabbir.photostore.base
+
+/**
+ * Base Class for handling errors/failures/exceptions.
+ * Every feature specific failure should extend [FeatureFailure] class.
+ */
+
+const val NO_INTERNET = "Device is not connected to the internet"
+const val NOT_LOGGED_IN = "You are not logged in"
+const val SOMETHING_WENT_WRONG = "Something went wrong"
+
+sealed class Failure
+
+/*Networking Specific*/
+class NetworkConnection(val message: String = NO_INTERNET) : Failure()
+class UnauthorizedError(val message: String = NOT_LOGGED_IN) : Failure()
+class CanNotConnectToServer(val message : String = "Can't connect to server."): Failure()
+
+/*Exception/Errors*/
+class ExceptionF(val message: String = SOMETHING_WENT_WRONG) : Failure()
+
+
+/*Feature Specific*/
+sealed class FeatureFailure : Failure()
+class APIKeyMissing(val message: String = "API Key Missing") : FeatureFailure()
