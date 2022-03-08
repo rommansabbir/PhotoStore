e90eeaba42f3a6e6d84fe2ed0623e5bd849542bd
Romman Sabbir <rommansabbir@gmail.com>
2022-03-08 20:09:54 +0600

    Extensions -> - Added extensions functions for SearchView

--- app/src/main/java/com/rommansabbir/photostore/base/Extensions.kt
+++ app/src/main/java/com/rommansabbir/photostore/base/Extensions.kt
@@ -2,7 +2,10 @@ package com.rommansabbir.photostore.base
 
 import android.graphics.drawable.Drawable
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -10,6 +13,8 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
+import com.rommansabbir.lazyloadingrecyclerview.handlerPostDelayed
+import com.rommansabbir.photostore.R
 
 inline fun <T> executeBodyOrReturnNull(crossinline body: () -> T): T? {
     return try {
@@ -55,4 +60,56 @@ fun ImageView.loadWithGlide(url: String, callback: (exception: GlideException?) 
             })
             .into(this)
     }
+}
+
+
+fun SearchView.customize() {
+    val editText =
+        this.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+    editText.setHintTextColor(
+        ContextCompat.getColor(
+            context,
+            R.color.black
+        )
+    )
+    editText.setTextColor(
+        ContextCompat.getColor(
+            context,
+            R.color.black
+        )
+    )
+    editText.textSize = 14f
+}
+
+/**
+ * Add an action which will be invoked when the text is changing.
+ *
+ * @return the [SearchView.OnQueryTextListener] added to the [SearchView]
+ */
+inline fun SearchView.doAfterTextChanged(
+    delay: Long = 500,
+    crossinline onTextChangedDelayed: (text: String) -> Unit
+) = doOnQueryTextListener(delay, onTextChangedDelayed)
+
+
+/**
+ * Add an action which will be invoked after the text changed.
+ *
+ * @return the [SearchView.OnQueryTextListener] added to the [SearchView]
+ */
+inline fun SearchView.doOnQueryTextListener(
+    delay: Long,
+    crossinline onTextChangedDelayed: (text: String) -> Unit
+): SearchView.OnQueryTextListener {
+    val queryListener = object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean = true
+        override fun onQueryTextChange(newText: String?): Boolean {
+            handlerPostDelayed(delay) {
+                onTextChangedDelayed.invoke(newText ?: "")
+            }
+            return true
+        }
+    }
+    this.setOnQueryTextListener(queryListener)
+    return queryListener
 }
