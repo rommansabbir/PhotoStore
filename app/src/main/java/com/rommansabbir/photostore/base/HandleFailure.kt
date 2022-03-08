ffa21e8932528a4ddfdb7e12ae430c7b666f4bf4
Romman Sabbir <rommansabbir@gmail.com>
2022-03-08 19:47:45 +0600

    HandleFailure -> - `handleFailure()` to handle failure - `showToast()` to show toast to the user

---
+++ app/src/main/java/com/rommansabbir/photostore/base/HandleFailure.kt
@@ -0,0 +1,18 @@
+package com.rommansabbir.photostore.base
+
+import android.content.Context
+import android.widget.Toast
+
+fun handleFailure(context: Context, failure: Failure) {
+    when (failure) {
+        is CanNotConnectToServer -> showToast(context, failure.message)
+        is ExceptionF -> showToast(context, failure.message)
+        is APIKeyMissing -> showToast(context, failure.message)
+        is NetworkConnectionError -> showToast(context, failure.message)
+        is UnauthorizedError -> showToast(context, failure.message)
+    }
+}
+
+fun showToast(context: Context, msg: String) {
+    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+}
